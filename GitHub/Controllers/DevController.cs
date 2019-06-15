d42a51b211bc889ca3f9ebc2c5f394f802d848bf
root <admin@nuosoon.com>
2019-06-16 01:48:50 +0800

    ADD

---
+++ GitHub/Controllers/DevController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using Vli.Helper;
+
+namespace GitHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevController : ControllerBase
+    {
+        [HttpPost]
+        public string nuosoon()
+        {
+            CmdHelper cmd = new CmdHelper();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(@"cd C:\Code\Vli\Vli.sln");
+            stringBuilder.Append("\n");
+            stringBuilder.Append("dotnet build");
+
+            cmd.Execute(stringBuilder.ToString());
+
+            return Guid.NewGuid().ToString();
+        }
+
+        [HttpGet]
+        public string nuosoo()
+        {
+            CmdHelper cmd = new CmdHelper();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(@"cd C:\Code\Vli\Vli.sln");
+            stringBuilder.Append("\n");
+            stringBuilder.Append("dotnet build");
+
+            cmd.Execute(stringBuilder.ToString());
+
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
